31698a33d5f17c339d0a8ed3a184113073c3269f
Morgan Glover <glovermo@bc.edu>
2019-08-26 10:37:26 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Morgan Glover on 8/26/19.
+//  Copyright © 2019 Morgan Glover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
