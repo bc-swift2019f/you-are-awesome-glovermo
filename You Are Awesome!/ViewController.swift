946f9ea8b47b2eda0f70a6f11c9dd52719b562a8
Morgan Glover <glovermo@bc.edu>
2019-09-02 09:55:06 -0400

    2.10 Complete - Index Added

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
     
     // Code below executes when the app's view first loads
     override func viewDidLoad() {
@@ -19,18 +20,36 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let message1 = "You Are Awesome!"
-        let message2 = "You Are Great!"
-        let message3 = "You Are Amazing!"
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "When the Genius Bar needs help, they call you",
+                        "You brighten my day!",
+                        "You are da bomb!",
+                        "Hey, fabulous!",
+                        "You are tremendous!",
+                        "You've got the design skills of Jony Ive!",
+                        "I can't wait to download your app!"]
         
-        
-        if messageLabel.text == message1 {
-            messageLabel.text = message2
-        } else if messageLabel.text == message2 {
-            messageLabel.text = message3
+        messageLabel.text = messages[index]
+        if index == (messages.count-1) {
+            index = 0
         } else {
-            messageLabel.text = message1
+            index += 1
         }
+        
+//        let message1 = "You Are Fantastic!!!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing!"
+//
+//
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        } else {
+//            messageLabel.text = message1
+//        }
     }
     
 }
