ad300ad24575fdc960b218c6eb8593e73763f198
Morgan Glover <glovermo@bc.edu>
2019-09-09 10:14:54 -0400

    2.19 Completed - inout Parameters

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -34,12 +34,12 @@ class ViewController: UIViewController {
         return newIndex
     }
     
-    func playSound(soundName: String) {
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
         
         if let sound = NSDataAsset(name: soundName) {
             do {
-                try awesomePlayer = AVAudioPlayer(data: sound.data)
-                awesomePlayer.play()
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
             } catch {
                 print("ERROR: data in \(soundName) could not be played as a sound.")
             }
@@ -71,7 +71,7 @@ class ViewController: UIViewController {
         // play a sound
         soundIndex = nonRepeatingRandom(lastNumber: soundIndex, maxValue: numberOfSounds)
         let soundName = "sound\(soundIndex)"
-        playSound(soundName: soundName)
+        playSound(soundName: soundName, audioPlayer: &awesomePlayer)
         
     }
     
